f3d2cc5a6607abc4377f7c4fde694abfb4291da2
sole <sole@mozilla.com>
2014-06-16 11:55:31 +0100

    implement image preloading to avoid flashy effects with slower servers such as simplehttpserver

--- dist/bespoke-fullscreenbackground.js
+++ dist/bespoke-fullscreenbackground.js
@@ -1,5 +1,5 @@
 /*!
- * bespoke-fullscreenbackground v0.0.0
+ * bespoke-fullscreenbackground v0.0.2
  * https://github.com/sole/bespoke-fullscreenbackground
  *
  * Copyright 2014, sole
@@ -12,11 +12,25 @@
   
   bespoke.plugins.fullscreenbackground = function(deck) {
 
+    var fsBgAttribute = 'data-bespoke-fullscreenbackground';
+    var preloadDiv = document.createElement('div');
+
+    // Preload the images to prevent "flashy" effects
+    deck.slides.forEach(function(slide) {
+      var imgSrc = slide.getAttribute(fsBgAttribute);
+      if(imgSrc) {
+        var img = document.createElement('img');
+        preloadDiv.appendChild(img);
+        img.src = imgSrc;
+      }
+
+    });
+
     deck.on('activate', function(e) {
       var index = e.index;
       var slide = e.slide;
       var deckParent = deck.parent;
-      var background = slide.getAttribute('data-bespoke-fullscreenbackground');
+      var background = slide.getAttribute(fsBgAttribute);
 
       if(background) {
         deckParent.classList.add('cover');
--- src/bespoke-fullscreenbackground.js
+++ src/bespoke-fullscreenbackground.js
@@ -4,11 +4,25 @@
   
   bespoke.plugins.fullscreenbackground = function(deck) {
 
+    var fsBgAttribute = 'data-bespoke-fullscreenbackground';
+    var preloadDiv = document.createElement('div');
+
+    // Preload the images to prevent "flashy" effects
+    deck.slides.forEach(function(slide) {
+      var imgSrc = slide.getAttribute(fsBgAttribute);
+      if(imgSrc) {
+        var img = document.createElement('img');
+        preloadDiv.appendChild(img);
+        img.src = imgSrc;
+      }
+
+    });
+
     deck.on('activate', function(e) {
       var index = e.index;
       var slide = e.slide;
       var deckParent = deck.parent;
-      var background = slide.getAttribute('data-bespoke-fullscreenbackground');
+      var background = slide.getAttribute(fsBgAttribute);
 
       if(background) {
         deckParent.classList.add('cover');
